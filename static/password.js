d49ff8abca329962c000bf4aeecc05da8cb33ecb
harrisuddin <harris7001@gmail.com>
2019-09-24 16:00:38 +0100

    implemented isValid function and declared other functions

--- static/password.js
+++ static/password.js
@@ -6,37 +6,42 @@ class Password {
 
     isValid() {
         if (containsSpecial(this.password)) {
-            // return error
+            return "Error, password shouldn't contain special characters";
+        } else if (this.password < 8) {
+            return "Error, password is less than 8 characters";
+        } else if (!containsNumber(this.password)) {
+            return "Error, password does not have a number";
+        } else if (!containsUpperCase(this.password)) {
+            return "Error, password does not have a capital letter";
+        } else if (!containsLowerCase(this.password)) {
+            return "Error, password does not have a lower case letter";
+        } else {
+            return true;
         }
     }
 }
 
 // implement functions to check certain conditions here
 
+// return true if s contains a special character
 function containsSpecial(s) {
 
 }
 
+// return true if s contains a number
+function containsNumber(s) {
 
+}
 
+// return true if s contains a capital letter
+function containsUpperCase(s) {
 
+}
 
+// return true if s contains a lower case
+function containsLowerCase(s) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 // export the class for use elsewhere
 module.exports = Password;
