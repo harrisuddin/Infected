d31d2b70cd78021624aff8aefc39ffe8207675fc
AaronMolesbury <55638411+AaronMolesbury@users.noreply.github.com>
2019-09-25 16:46:12 +0100

    Implemented password checks
    checks for upper&lowercase, special and numerical characters

--- static/password.js
+++ static/password.js
@@ -25,22 +25,50 @@ class Password {
 
 // return true if s contains a special character
 function containsSpecial(s) {
-
+  var specialCharacters = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
+  for(i=0;i<s.length;i++){
+    if(string.indexOf(specialCharacters[i])>-1){
+      return true;
+    }
+  }
+  return false;
 }
 
 // return true if s contains a number
 function containsNumber(s) {
-
+  if(s.includes("0")){
+    return true;
+  } else if(s.includes("1")){
+    return true;
+  } else if(s.includes("2")){
+    return true;
+  } else if(s.includes("3")){
+    return true;
+  } else if(s.includes("4")){
+    return true;
+  } else if(s.includes("5")){
+    return true;
+  } else if(s.includes("6")){
+    return true;
+  } else if(s.includes("7")){
+    return true;
+  } else if(s.includes("8")){
+    return true;
+  } else if(s.includes("9")){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // return true if s contains a capital letter
 function containsUpperCase(s) {
-
+  return str == str.toUpperCase() && str != str.toLowerCase();
 }
 
 // return true if s contains a lower case
 function containsLowerCase(s) {
-
+  return str == str.toLowerCase() && str != str.toUpperCase();
 }
 
 // export the class for use elsewhere
