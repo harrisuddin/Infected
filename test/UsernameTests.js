1e3630243593f3e2ad864ae0c72ff36e5db96819
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2019-09-25 17:43:32 +0100

    Add files via upload
    This will test if the username is bigger than 20 characters and if it's smaller than 1 character.

---
+++ test/UsernameTests.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+const Username = require('../static/password.js');
+var assert = chai.assert;
+
+describe('Username', function() {
+
+    it('should return an error message if username is bigger than 20 characters', function() {
+        var name = new Username("thisgameisgoingtobethebestgame");
+        assert.equal(name.isValid(), "Username cant be bigger than 20 characters long, please try again.");
+    });
+    
+    it('should return an error message if username is smaller than 1 characters', function() {
+        var name = new Username("");
+        assert.equal(name.isValid(), "Username cant be smaller than 1 characters long, please try again.");
+    });
+
+    it('should return true if username is valid', function() {
+        var name = new Username("Aaron");
+        assert.equal(name.isValid(), true);
+    });
+
+});
