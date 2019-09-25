2f6f9025cd96ffa8cda09afc168c40b5be651072
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2019-09-25 17:27:57 +0100

    Username
    js for the username of the players and checks that the username is smaller than 20 characters

---
+++ static/Username.js
@@ -0,0 +1,18 @@
+class Username {
+
+    constructor(string) { //ceats a contructor with type string
+        this.Username = string;
+    }
+
+    isValid() {
+        if (this.Username.length > 20) { //a error message will apear to the player if the user name is longer than 20 characters.
+            return "Username cant be bigger than 20 characters long, please try again.";
+        } esle {
+            return true;
+        }
+    }
+}
+
+
+// exports the class to a differnt location
+module.exports = Username;
