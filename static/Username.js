ca27ac818f560d3b9580d243a337cad68616ac03
harrisuddin <harris7001@gmail.com>
2019-09-25 20:49:32 +0100

    edited comments

--- static/Username.js
+++ static/Username.js
@@ -1,20 +1,18 @@
 class Username {
 
-    constructor(string) { //ceats a contructor with type string
-        this.Username = string;
+    constructor(string) { // creates a constructor with a string parameter
+        this.username = string;
     }
 
     isValid() {
-        if (this.Username.length > 20) { //an error message will apear to the player if the user name is longer than 20 characters.
+        if (this.username.length > 20) { //an error message will appear to the player if the user name is longer than 20 characters.
             return "Username cant be bigger than 20 characters long, please try again.";
-        }  else if(this.Username.length < 1) { //a error message will apear to the player if the user name is smaller than 1 characters.
-           return "Username cant be smaller than 1 characters long, please try again.";
+        } else if (this.username.length < 1) { //a error message will apear to the player if the user name is smaller than 1 characters.
+            return "Username cant be smaller than 1 characters long, please try again.";
         }
-            return true;
-        }
+        return true;
     }
 }
-
 
 // exports the class to a differnt location
 module.exports = Username;
