4b333a0cad76543a7684ffff2f81bbaea6ae36ab
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2019-09-26 14:34:29 +0100

    comments

--- test/usernameTests.js
+++ test/usernameTests.js
@@ -3,7 +3,7 @@ const Username = require('../static/username.js');
 var assert = chai.assert;
 
 describe('Username', function() {
-
+    //This will test if the username is bigger than 20 chars and should return a error message if its bigger than 20 chars
     it('should return an error message if username is bigger than 20 characters', function() {
         var name = new Username("thisgameisgoingtobethebestgame");
         assert.equal(name.isValid(), "Username cant be bigger than 20 characters long, please try again.");
