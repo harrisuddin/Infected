d52af4be6c9c05afac6f8851fc3fc640a3edc1e4
harrisuddin <harris7001@gmail.com>
2019-10-08 10:21:42 +0100

    implemented player rotation

--- game/index.js
+++ game/index.js
@@ -1,6 +1,10 @@
 // Globals
-var x = 10;
-var y = 10;
+var player = new Player();
+var x = player.getX();
+var y = player.getY();
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("playerI");
 var playerMovement = {
     up: false,
     down: false,
@@ -15,24 +19,22 @@ function play() {
 }
 
 function update() {
+    // Test
     console.log(x + " " + y);
+
     document.addEventListener('keydown', function (event) {
         switch (event.keyCode) {
             case 65: // A
                 playerMovement.left = true;
-                //x -= 5;
                 break;
             case 87: // W
                 playerMovement.up = true;
-                //y -= 5;
                 break;
             case 68: // D
                 playerMovement.right = true;
-                //x += 5;
                 break;
             case 83: // S
                 playerMovement.down = true;
-                //y += 5;
                 break;
         }
     });
@@ -65,13 +67,79 @@ function update() {
     if (playerMovement.down) {
         y += 5;
     }
-    drawPlayer(x, y);
+
+    var degree = returnPlayerRotation();
+    drawPlayer(img, x, y, 75, 100, degree);
 }
 
-function drawPlayer(x, y) {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, 300, 300);
-    var img = document.getElementById("playerI");
-    ctx.drawImage(img, x, y, 50, 37.5);
+function drawPlayer(img, x, y, width, height, deg) {
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations#Basic_animation_steps
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, 700, 700);
+
+    // Store the current context state (i.e. rotation, translation etc..)
+    ctx.save()
+
+    // Convert degrees to radian
+    var radian = deg * Math.PI / 180;
+
+    // Set the origin to the center of the image
+    ctx.translate(x + width / 2, y + height / 2);
+
+    // Rotate the canvas around the origin
+    ctx.rotate(radian);
+
+    // Draw the image
+    ctx.drawImage(img, width / 2 * (-1), height / 2 * (-1), width, height);
+
+    // Restore canvas state as saved from above
+    ctx.restore();
+}
+
+// Return the player rotation in degrees
+function returnPlayerRotation() {
+
+    if (playerMovement.up) {
+
+        if (playerMovement.right || playerMovement.left) {
+            // And they are pressing D
+            if (playerMovement.right) {
+                return 45;
+            }
+            // And they are pressing A
+            if (playerMovement.left) {
+                return 315;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    if (playerMovement.down) {
+
+        if (playerMovement.right || playerMovement.left) {
+            // And they are pressing D
+            if (playerMovement.right) {
+                return 135;
+            }
+            // And they are pressing A
+            if (playerMovement.left) {
+                return 225;
+            }
+        } else {
+            return 180;
+        }
+    }
+
+    if (playerMovement.left) {
+        return 270;
+    }
+
+    if (playerMovement.right) {
+        return 90;
+    }
+
+    return 0;
 }
