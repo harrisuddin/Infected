e0e4f6cc0d654d4bf8be31c531901d67ebff8e59
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2019-10-10 15:05:49 +0100

    javascript for button

---
+++ buttonjs.js
@@ -0,0 +1,6 @@
+
+function signupfun() {
+  document.getElementById("signup").style.display="block";
+  document.getElementById("Lbutton").style.display="none";
+  console.log(document.getElementById("signup"));
+}
