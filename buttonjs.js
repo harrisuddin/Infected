69240f00ca60ffc403f6d78a6d3d7ce35a5ca50a
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2019-11-06 11:48:01 +0000

    fixed bugs

--- buttonjs.js
+++ buttonjs.js
@@ -1,8 +1,7 @@
-
 var Username;
 var Password;
 var Password2;
-var Signupvalid = true;
+var Signupvalid = false;
 
 
 function Startup(){
@@ -12,48 +11,61 @@ function Startup(){
 //this function is to change the homepage to be able to signup the user
 function signupfun() {
   document.getElementById("Singupb").setAttribute("onClick", "signupfun2()");
-  document.getElementById("pw2").style.display="block";
+  document.getElementById("pw2").style.display="inline";
   document.getElementById("Lbutton").value="OR LOGIN";
+  document.getElementById("Lbutton").setAttribute("onClick", "orlogin()");
 }
 
 //this function is for when the user is siging up
 function signupfun2(){
+  var Username = document.getElementById("un").value;
+  var Password = document.getElementById("pw").value;
+  var Password2 = document.getElementById("pw2").value;
 if (Username=="" || Username.length>10){
   alert("Username has to be between 0 to 10 characters");
+  Signupvalid = false;
 } else {
 document.getElementById("testun").innerHTML = Username;
+Signupvalid = true;
 }
 
 if (Password=="" || Password.length>10){
   alert("Password has to be between 0 to 10 characters");
+  Signupvalid = false;
 } else {
   document.getElementById("testpw").innerHTML = Password;
+  Signupvalid = true;
 }
 
 if (Password=="" || Password.length>10){
   alert("Re-enter Password has to be between 0 to 10 characters");
+  Signupvalid = false;
 } else {
   document.getElementById("testpw2").innerHTML = Password2;
+  Signupvalid = true;
 }
 
 if(Password==Password2){
-return true;
+  Signupvalid = true;
 }
-else{
+  else{
 alert("password must be same!");
+Signupvalid = false;
 }
-  if (Signup_valid == true) {
+  if (Signupvalid == true) {
     document.getElementById("startpagebox").style.display="none";
   }
 }
 
+function orlogin(){
+  document.getElementById("pw2").style.display="none";
+  document.getElementById("Lbutton").value="LOGIN";
+  document.getElementById("Singupb").setAttribute("onClick", "signupfun()");
+  document.getElementById("Lbutton").setAttribute("onClick", "loginfun()");
+}
+
+
 function loginfun() {
-  if (document.getElementById("Lbutton").value="OR LOGIN") {
-    document.getElementById("pw2").style.display="none";
-    document.getElementById("Lbutton").value="LOGIN";
-    document.getElementById("Singupb").setAttribute("onClick", "signupfun()");
-  }
-    if (document.getElementById("Lbutton").value="LOGIN") {
       var Username = document.getElementById("un").value;
       var Password = document.getElementById("pw").value;
       var Password2 = document.getElementById("pw2").value;
@@ -61,8 +73,6 @@ function loginfun() {
   document.getElementById("testpw").innerHTML = Password;
   document.getElementById("testpw2").innerHTML = Password2;
   document.getElementById("startpagebox").style.display="none";
-}
-
 }
 
 
