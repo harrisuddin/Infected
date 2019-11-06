e3adb3e670d49aa0bb64d6b0fdd7b5ee367926d2
harrisuddin <harris7001@gmail.com>
2019-11-06 12:01:31 +0000

    deleted Lucas folder

--- Lucas/buttonjs.js
+++
@@ -1,6 +0,0 @@
-
-function signupfun() {
-  document.getElementById("signup").style.display="block";
-  document.getElementById("Lbutton").style.display="none";
-  console.log(document.getElementById("signup"));
-}
