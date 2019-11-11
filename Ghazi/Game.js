49bd8315ba2280aea5d52081021c56cae07354c5
Ghazi Yusaf <ghaziyusaf@gmail.com>
2019-11-11 11:58:58 +0000

    Added wall collision
    The player can no longer leave the canvas
    
    Signed-off-by: Ghazi Yusaf <ghaziyusaf@gmail.com>

--- Ghazi/Game.js
+++ Ghazi/Game.js
@@ -26,42 +26,42 @@ player2.image.onload = animate;
 
 function animate() 
 {
-    if (keyHandler.upPressed) 
+    if (keyHandler.upPressed && player.yPosition > 0) 
     {
         player.yPosition -= player.speed;
         player.rotation = 0;
-        if (keyHandler.rightPressed) 
+        if (keyHandler.rightPressed && player.xPosition < canvas.width - player.image.width - 60) 
         {
             player.xPosition += player.speed;
             player.rotation = 45;
         }
-        else if (keyHandler.leftPressed) 
+        else if (keyHandler.leftPressed && player.xPosition > 0) 
         {
             player.xPosition -= player.speed;
             player.rotation = 315;
         }
     }
-    else if (keyHandler.rightPressed) 
+    else if (keyHandler.rightPressed && player.xPosition < canvas.width - player.image.width - 60) 
     {
         player.xPosition += player.speed;
         player.rotation = 90;
-        if (keyHandler.downPressed) 
+        if (keyHandler.downPressed && player.yPosition < canvas.height - player.image.height - 60) 
         {
             player.yPosition += player.speed;
             player.rotation = 135;
         }
     }
-    else if (keyHandler.downPressed) 
+    else if (keyHandler.downPressed && player.yPosition < canvas.height - player.image.height - 60) 
     {
         player.yPosition += player.speed;
         player.rotation = 180;
-        if (keyHandler.leftPressed) 
+        if (keyHandler.leftPressed && player.xPosition > 0) 
         {
             player.xPosition -= player.speed;
             player.rotation = 225;
         }
     }
-    else if (keyHandler.leftPressed) 
+    else if (keyHandler.leftPressed && player.xPosition > 0) 
     {
         player.xPosition -= player.speed;
         player.rotation = 270;
@@ -76,11 +76,13 @@ function draw()
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     context.save();
+
     context.translate(player.xPosition + player.image.width, player.yPosition + player.image.height);
     context.rotate(player.rotation * Math.PI / 180);
     context.drawImage(player.image, -(player.image.width / 2), -(player.image.height / 2));
+
     context.restore();
-    context.fillText(player.username, player.xPosition + 25, player.yPosition + 25);
+
+    context.fillText(player.username, player.xPosition + 35, player.yPosition + 35);
     context.drawImage(player2.image, player2.xPosition, player2.yPosition);
-    
 }
