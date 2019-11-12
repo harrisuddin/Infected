764a7962eefedec7bf6fc9833c8a7bef3568f626
Ghazi Yusaf <ghaziyusaf@gmail.com>
2019-11-12 11:54:33 +0000

    Clean up
    Cleaned up Game.js and added an animator class to handle drawing elements to the canvas
    
    Signed-off-by: Ghazi Yusaf <ghaziyusaf@gmail.com>

---
+++ Ghazi/Animator.js
@@ -0,0 +1,122 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+canvas.height = window.innerHeight - 1;
+canvas.width = window.innerWidth - 1;
+
+context.font = "18px Sans-Serif";
+
+keyHandler = new KeyHandler();
+document.addEventListener("keydown", (event) => 
+{
+    
+keyHandler.keyDownHandler(event);
+}, false);
+document.addEventListener("keyup", (event) => 
+{
+    
+keyHandler.keyUpHandler(event);
+}, false);
+
+class Animator 
+{
+    constructor(player, AI) 
+    {
+        this._player = player;
+        this._AI = AI;
+    }
+
+    draw() 
+    {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        this.rotate();
+        this.animate();
+
+        context.fillText(this._player.username, this._player.xPosition + 40, this._player.yPosition + 45);
+        
+        this.drawAI();
+    }
+
+    animate() 
+    {
+        if (keyHandler.upPressed && this._player.yPosition > 0) 
+        {
+            this._player.yPosition -= this._player.speed;
+            this._player.rotation = 0;
+            if (keyHandler.rightPressed && this._player.xPosition < canvas.width - this._player.image.width - 60) 
+            {
+                this._player.xPosition += this._player.speed;
+                this._player.rotation = 45;
+            }
+            else if (keyHandler.leftPressed && this._player.xPosition > 0) 
+            {
+                this._player.xPosition -= this._player.speed;
+                this._player.rotation = 315;
+            }
+        }
+        else if (keyHandler.rightPressed && this._player.xPosition < canvas.width - this._player.image.width - 60) 
+        {
+            this._player.xPosition += this._player.speed;
+            this._player.rotation = 90;
+            if (keyHandler.downPressed && this._player.yPosition < canvas.height - this._player.image.height - 60) 
+            {
+                this._player.yPosition += this._player.speed;
+                this._player.rotation = 135;
+            }
+        }
+        else if (keyHandler.downPressed && this._player.yPosition < canvas.height - this._player.image.height - 60) 
+        {
+            this._player.yPosition += this._player.speed;
+            this._player.rotation = 180;
+            if (keyHandler.leftPressed && this._player.xPosition > 0) 
+            {
+                this._player.xPosition -= this._player.speed;
+                this._player.rotation = 225;
+            }
+        }
+        else if (keyHandler.leftPressed && this._player.xPosition > 0) 
+        {
+            this._player.xPosition -= this._player.speed;
+            this._player.rotation = 270;
+        }
+    }
+
+    drawAI() 
+    {
+        this._AI.forEach(AIElement => {
+            context.drawImage(AIElement.image, AIElement.xPosition, AIElement.yPosition);
+
+            if (this._player.rotation == 90 || this._player.rotation == 270) 
+            {
+                if (this._player.xPosition <= AIElement.xPosition + AIElement.image.width - 30 && this._player.xPosition + this._player.image.height >= AIElement.xPosition - 20) 
+                {
+                    if (this._player.yPosition <= AIElement.yPosition + AIElement.image.height - 60 && this._player.yPosition + this._player.image.width >= AIElement.yPosition - 60) 
+                    {
+                        AIElement.isInfected = true;
+                    }
+                }
+            }
+            else 
+            {
+                if (this._player.xPosition <= AIElement.xPosition + AIElement.image.width - 25 && this._player.xPosition + this._player.image.width >= AIElement.xPosition - 25) 
+                {
+                    if (this._player.yPosition <= AIElement.yPosition + AIElement.image.height - 55 && this._player.yPosition + this._player.image.height >= AIElement.yPosition - 40) 
+                    {
+                        AIElement.isInfected = true;
+                    }
+                }
+            }
+        });
+    }
+
+    rotate() 
+    {
+        context.save();
+
+        context.translate(this._player.xPosition + this._player.image.width, this._player.yPosition + this._player.image.height);
+        context.rotate(this._player.rotation * Math.PI / 180);
+        context.drawImage(this._player.image, -(this._player.image.width / 2), -(this._player.image.height / 2));
+
+        context.restore();
+    }
+}
--- Ghazi/Game.js
+++ Ghazi/Game.js
@@ -1,124 +1,22 @@
-var canvas = document.getElementById("gameCanvas");
-var context = canvas.getContext("2d");
-canvas.height = window.innerHeight - 1;
-canvas.width = window.innerWidth - 1;
-
-context.font = "18px Sans-Serif";
-
-const keyHandler = new KeyHandler();
-document.addEventListener("keydown", (event) => 
-{
-    keyHandler.keyDownHandler(event);
-}, false);
-document.addEventListener("keyup", (event) => 
-{
-    keyHandler.keyUpHandler(event);
-}, false);
-
 const player = new Player("Infected", 1200, 100, true);
 
 const AI = [];
-
 AI[0] = new Player("Human1", 300, 700, false);
 AI[1] = new Player("Human2", 600, 500, false);
 AI[2] = new Player("Human4", 1000, 300, false);
 AI[3] = new Player("Human3", 1200, 900, false);
 
-player.image.onload = animate;
+const animator = new Animator(player, AI);
 
-function animate() 
+player.image.onload = gameRun;
+
+function gameRun() 
 {
-    if (keyHandler.upPressed && player.yPosition > 0) 
-    {
-        player.yPosition -= player.speed;
-        player.rotation = 0;
-        if (keyHandler.rightPressed && player.xPosition < canvas.width - player.image.width - 60) 
-        {
-            player.xPosition += player.speed;
-            player.rotation = 45;
-        }
-        else if (keyHandler.leftPressed && player.xPosition > 0) 
-        {
-            player.xPosition -= player.speed;
-            player.rotation = 315;
-        }
-    }
-    else if (keyHandler.rightPressed && player.xPosition < canvas.width - player.image.width - 60) 
-    {
-        player.xPosition += player.speed;
-        player.rotation = 90;
-        if (keyHandler.downPressed && player.yPosition < canvas.height - player.image.height - 60) 
-        {
-            player.yPosition += player.speed;
-            player.rotation = 135;
-        }
-    }
-    else if (keyHandler.downPressed && player.yPosition < canvas.height - player.image.height - 60) 
-    {
-        player.yPosition += player.speed;
-        player.rotation = 180;
-        if (keyHandler.leftPressed && player.xPosition > 0) 
-        {
-            player.xPosition -= player.speed;
-            player.rotation = 225;
-        }
-    }
-    else if (keyHandler.leftPressed && player.xPosition > 0) 
-    {
-        player.xPosition -= player.speed;
-        player.rotation = 270;
-    }
-
-    draw();
-    requestAnimationFrame(animate);
+    update();
+    requestAnimationFrame(gameRun);
 }
 
-function drawAI() 
+function update() 
 {
-    AI.forEach(AIElement => {
-        context.drawImage(AIElement.image, AIElement.xPosition, AIElement.yPosition);
-
-        if (player.rotation == 90 || player.rotation == 270) 
-        {
-            if (player.xPosition <= AIElement.xPosition + AIElement.image.width - 30 && player.xPosition + player.image.height >= AIElement.xPosition - 20) 
-            {
-                if (player.yPosition <= AIElement.yPosition + AIElement.image.height - 60 && player.yPosition + player.image.width >= AIElement.yPosition - 60) 
-                {
-                    AIElement.isInfected = true;
-                }
-            }
-        }
-        else 
-        {
-            if (player.xPosition <= AIElement.xPosition + AIElement.image.width - 25 && player.xPosition + player.image.width >= AIElement.xPosition - 25) 
-            {
-                if (player.yPosition <= AIElement.yPosition + AIElement.image.height - 55 && player.yPosition + player.image.height >= AIElement.yPosition - 40) 
-                {
-                    AIElement.isInfected = true;
-                }
-            }
-        }
-    });
-}
-
-function rotate() 
-{
-    context.save();
-
-    context.translate(player.xPosition + player.image.width, player.yPosition + player.image.height);
-    context.rotate(player.rotation * Math.PI / 180);
-    context.drawImage(player.image, -(player.image.width / 2), -(player.image.height / 2));
-
-    context.restore();
-}
-
-function draw() 
-{
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    rotate();
-
-    context.fillText(player.username, player.xPosition + 35, player.yPosition + 35);
-    
-    drawAI();
+    animator.draw();
 }
