3560de3c810feae1840892c10852976a9d8c20b7
rl68uni <55630040+rl68uni@users.noreply.github.com>
2019-11-14 13:13:29 +0000

    Made Database Class

--- sql/db.js
+++ sql/db.js
@@ -1,23 +1,40 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: "sql2.freesqldatabase.com",
-  user: "sql2310973",
-  password: "bD5%zW5!"
-  database: "sql2310973"
-});
+class Database {
 
-connection.connect(function(err) {
- if (err) throw err;
-  console.log("Connected!");
-  var createUsers = "CREATE TABLE IF NOT EXISTS Users(username VARCHAR(20) PRIMARY KEY, password VARCHAR(256), id VARCHAR(64)) ENGINE=INNODB;"
-  var createScores = "CREATE TABLE IF NOT EXISTS Scores(name VARCHAR(20), score INT, FOREIGN KEY(name) REFERENCES Users(username))ENGINE=INNODB;"
-  connection.query(createUsers, function (err, result) {
-    if (err) throw err;
-    console.log("User table created");
-  });
-  connection.query(createScores, function (err, result) {
-    if (err) throw err;
-    console.log("Score table created");
-  });
-});
+  constructor(host, user, password, database) {
+
+    this._host = host;
+    this._user = user;
+    this._password = password;
+    this._database = database;
+    this._connection = mysql.createConnection({ host: host, user: user, password: password, database: database });
+  }
+
+  runQuery(query) {
+
+    this._connection.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+      connection.query(query, function (err, result) {
+        if (err) throw err;
+        console.log("Query Success");
+      });
+    });
+
+  }
+
+// connection.connect(function(err) {
+//  if (err) throw err;
+//   console.log("Connected!");
+//   var createUsers = "CREATE TABLE IF NOT EXISTS Users(username VARCHAR(20) PRIMARY KEY, password VARCHAR(256), id VARCHAR(64)) ENGINE=INNODB;"
+//   var createScores = "CREATE TABLE Scores(name VARCHAR(20), score INT, FOREIGN KEY(name) REFERENCES Users(username))ENGINE=INNODB;"
+//   connection.query(createUsers, function (err, result) {
+//     if (err) throw err;
+//     console.log("User table created");
+//   });
+//   connection.query(createScores, function (err, result) {
+//     if (err) throw err;
+//     console.log("Score table created");
+//   });
+// });
