9e445e9f4dc2b3a3fcb136179cb4f381b9cac61d
rl68uni <55630040+rl68uni@users.noreply.github.com>
2019-11-14 12:56:41 +0000

    Created queries to create tables

--- sql/db.js
+++ sql/db.js
@@ -4,9 +4,20 @@ var connection = mysql.createConnection({
   host: "sql2.freesqldatabase.com",
   user: "sql2310973",
   password: "bD5%zW5!"
+  database: "sql2310973"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
+ if (err) throw err;
   console.log("Connected!");
+  var createUsers = "CREATE TABLE IF NOT EXISTS Users(username VARCHAR(20) PRIMARY KEY, password VARCHAR(256), id VARCHAR(64)) ENGINE=INNODB;"
+  var createScores = "CREATE TABLE IF NOT EXISTS Scores(name VARCHAR(20), score INT, FOREIGN KEY(name) REFERENCES Users(username))ENGINE=INNODB;"
+  connection.query(createUsers, function (err, result) {
+    if (err) throw err;
+    console.log("User table created");
+  });
+  connection.query(createScores, function (err, result) {
+    if (err) throw err;
+    console.log("Score table created");
+  });
 });
