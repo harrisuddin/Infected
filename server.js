6a0b8419d23b52746b4ad7fac91c66fdc4389c78
harrisuddin <harris7001@gmail.com>
2019-11-14 16:17:46 +0000

    changed path names

--- server.js
+++ server.js
@@ -10,16 +10,17 @@ app.use('/sql', express.static(__dirname + '/sql'));
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/game', express.static(__dirname + '/game'));
+app.use('/Ghazi', express.static(__dirname + '/Ghazi'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/Ghazi/index.html'));
 });
 
 // temporary to test db connection
 app.get('/db', (req, res) => {
     var createUsers = "CREATE TABLE IF NOT EXISTS Users(username VARCHAR(20) PRIMARY KEY, password VARCHAR(256), id VARCHAR(64)) ENGINE=INNODB;"
-    var createScores = "CREATE TABLE IF NOT EXISTS Scores(name VARCHAR(20), score INT, FOREIGN KEY(name) REFERENCES Users(username))ENGINE=INNODB;"
+    var createScores = "CREATE TABLE IF NOT EXISTS Scores(name VARCHAR(20), score INT, FOREIGN KEY(name) REFERENCES Users(username)) ENGINE=INNODB;"
     db.query(createUsers, function (err, result) {
         if (err) throw err;
         console.log("User table created");
