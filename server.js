772357758ca9e010db50af73e9cb7a15bb388391
harrisuddin <harris7001@gmail.com>
2019-11-14 12:42:43 +0000

    added mysql and db connection

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Dependencies
 const express = require('express');
+const db = require('./sql/db.js');
 var path = require('path');
 const app = express();
 
 // allows the app to use any necessary folders
 app.use('/front-end', express.static(__dirname + '/front-end'));
+app.use('/sql', express.static(__dirname + '/sql'));
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/game', express.static(__dirname + '/game'));
@@ -14,7 +16,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-var port = process.env.PORT || 80;
+// temporary to test db connection
+app.get('/db', (req, res) => {
+   db.connect(function (err) {
+       if (err) throw err;
+       console.log("Connected!");
+   });
+});
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
