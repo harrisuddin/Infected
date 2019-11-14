ead804c1e23b0ef7faeaacee009ea731d77a0cdc
Lucasarenaterry <55638738+Lucasarenaterry@users.noreply.github.com>
2019-11-14 14:11:15 +0000

    Update game.js

--- game/game.js
+++ game/game.js
@@ -21,7 +21,6 @@ var playerMovement2 = {
 }
 
 function play() {
-    countdown();
     setInterval(function () {
         update();
     }, (1000 / 50));
@@ -234,64 +233,6 @@ function returnPlayerRotation(pm) {
     if (pm.right) {
         return 90;
     }
- //set minutes
-      var mins = 2;
-
-      //calculate the seconds
-      var secs = mins * 60;
-
-      function countdown() {
-          setTimeout('Decrement()', 1000);
-      }
-      //decreases time
-      function Decrement() {
-          if (document.getElementById) {
-              minutes = document.getElementById("minutes");
-              seconds = document.getElementById("seconds");
-
-              //if less than a minute remaining
-              //Display only seconds value.
-              if (seconds < 59) {
-                  seconds.value = secs;
-              }
-              //Display both minutes and seconds
-              else {
-                  minutes.value = getminutes();
-                  seconds.value = getseconds();
-              }
-              //when less than a minute remaining
-              //colour of the minutes and seconds
-              //changes to red
-              if (mins < 1) {
-                  minutes.style.color = "red";
-                  seconds.style.color = "red";
-              }
-              //if seconds becomes zero,
-              //then page alert time up
-              if (mins < 0) {
-                  alert('time up');
-                  minutes.value = 0;
-                  seconds.value = 0;
-              }
-              //if seconds > 0 then seconds is decremented
-              else {
-                  secs--;
-                  setTimeout('Decrement()', 1000);
-              }
-          }
-      }
-
-      function getminutes() {
-          //minutes is seconds divided by 60, rounded down
-          mins = Math.floor(secs / 60);
-          return mins;
-      }
-
-      function getseconds() {
-          //take minutes remaining (as seconds) away
-          //from total seconds remaining
-          return secs - Math.round(mins * 60);
-      }
 
     return 0;
 }
