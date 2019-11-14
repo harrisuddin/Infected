f5f28452f4e44e7302c1d14bc91daf9653e8a058
harrisuddin <harris7001@gmail.com>
2019-11-14 17:16:00 +0000

    added socket io

--- Ghazi/Game.js
+++ Ghazi/Game.js
@@ -1,7 +1,10 @@
 // This is the time of the game in seconds.
 var currentTime = 120;
+var socket = io();
 
 const player = new Player("Infected", 1200, 100, true);
+
+socket.emit('new player');
 
 const AI = [];
 AI[0] = new Player("Human1", 300, 700, false);
@@ -26,4 +29,20 @@ function gameTimer()
     currentTime--;
     document.getElementById("timer").innerHTML = "TIME REMAINING: " + currentTime;
 }
+
+setInterval(function () {
+    socket.emit('player', player);
+}, 1000);
+
+
+const player = new Player("Infected", 1200, 100, true);
+
+
+
+//Test connection is working
+//socket.on('message', function (data) {
+//    console.log(data);
+//});
+
+
 
